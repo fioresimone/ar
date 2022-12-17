1e08bb48eff737b5cbab6483ad4585b4df3fce2a
fioresimone <simonefiore88@gmail.com>
2022-12-17 15:10:51 +0100

    f

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
-import React, { Suspense, useState } from "react";
+import React, { Suspense, useRef, useState } from "react";
 import { Interactive, XR, ARButton, Controllers } from "@react-three/xr";
 import { Float, Sphere, Text } from "@react-three/drei";
 import "./index.css";
-import { Canvas } from "@react-three/fiber";
-import Model from "./Moon";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
+
+import MoonColor from "./color.jpeg";
+import MoonNormal from "./normal.jpg";
+import MoonBump from "./bump.jpg";
 
 export default function App() {
+  function Moon() {
+    const [color, normal, bump] = useLoader(TextureLoader, [
+      MoonColor,
+      MoonNormal,
+      MoonBump,
+    ]);
+    const ref = useRef();
+    useFrame((state) => {
+      const elapsed = state.clock.elapsedTime / 10;
+      ref.current.rotation.y = elapsed;
+    });
+
+    return (
+      <mesh ref={ref} position={[0, 0, 0]}>
+        <sphereGeometry args={[1, 256, 256]} />
+        <meshStandardMaterial
+          map={color}
+          bumpMap={bump}
+          bumpScale={0.01}
+          normalMap={normal}
+          normalScale={0.4}
+        />
+      </mesh>
+    );
+  }
+
   return (
     <>
       <ARButton />
       <Canvas>
         <XR referenceSpace="local">
           <ambientLight intensity={1} />
-          <Float>
-            <Model position={[0, 0, 1]} scale={0.3} />
-          </Float>
+          <Suspense fallback={null}>
+            <Moon />
+          </Suspense>
         </XR>
       </Canvas>
     </>
